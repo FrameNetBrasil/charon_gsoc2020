f9455003298a4d12a39b45198f82f0dc5dbaa750
Prishita Ray <prishita.r85@gmail.com>
2020-08-18 23:21:08 +0530

    Add sentence id to track_objects

--- public/server/track_objects.py
+++ public/server/track_objects.py
@@ -24,21 +24,34 @@ cors = CORS(app)
 @app.route('/track',methods=['GET','POST'])
 def track():
    fnames=os.listdir(PATH_TO_VIDEO_DIR)
-   fnames=sorted(fnames)
-   filename=fnames[0]
+   paths = [os.path.join(PATH_TO_VIDEO_DIR, basename) for basename in fnames]
+   filename=max(paths, key=os.path.getctime)
+   print(filename)
+   f=filename.split(".")
    start_time=request.form['start_time']
    end_time=request.form['end_time']
+   sid=request.form['sid']
+   sid=sid-1
    print(PATH_TO_VIDEO_DIR)
    video_path=os.path.join(PATH_TO_VIDEO_DIR,filename)
    print(video_path)
    v=0
+   
    #extract video clip from timestamps
-   objectTracking.detect_and_track(video_path,start_time,end_time,v)
+   origdir= config.get("charon", "dataPath")  
+   os.chdir(origdir+ 'Object_Store')
+   if os.path.isdir(f[0])==False:
+      os.mkdir(f[0])
+   os.chdir(f[0])
+   os.mkdir("sentence_"+sid)
+   os.chdir(origdir)
+   
+   objectTracking.detect_and_track(video_path,sid,start_time,end_time,v)
 
-   val=input("Enter yes if satisfied with detected objects and no to create and track own objects...")
-   if val=="no":
-      v=1
-      objectTracking.detect_and_track(video_path,start_time,end_time,v)
+   #val=input("Enter yes if satisfied with detected objects and no to create and track own objects...")
+   #if val=="no":
+      #v=1
+      #objectTracking.detect_and_track(video_path,start_time,end_time,v)
    return 'ok'
 
 if __name__=='__main__':
